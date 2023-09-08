d6d62326b004912a972711fcd176b0693bf4e540
charbel-mehou <charbel.mehou@groupe-esigelec.org>
2023-09-09 01:43:37 +0200

    ✨ Adding a new screen related to my projects

--- personal_portfolio/src/components/Footer.js
+++ personal_portfolio/src/components/Footer.js
@@ -1,20 +1,7 @@
-import GitHubIcon from '@mui/icons-material/GitHub'
-import LinkedInIcon from '@mui/icons-material/LinkedIn'
-import MailOutlineIcon from '@mui/icons-material/MailOutline'
 import { Box, Typography } from '@mui/material'
-import Button from '@mui/material/Button'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-const handleLInkedLnClick = () => {
-  window.open('https://www.linkedin.com/in/charbel-mehou/', '_blank')
-}
 
-const handleGithubClic=()=>{
-  window.open('https://github.com/CharbelMehou/FitnessApp','_blank')
-}
-const handleEmailClick = () => {
-  window.open('mailto:mehoucharbel@gmail.com?subject=Contact depuis le site Web', '_blank');
-}
 
 const Footer = () => {
   const {t} =useTranslation()
@@ -29,30 +16,7 @@ const Footer = () => {
     {t('copyright')} 
     </Typography>
 
-    <Box sx={{display:"flex",flexDirection:"row",justifyContent:'center',gap:1}}>
-      <Button
-        variant="contained"
-        onClick={handleLInkedLnClick}
-        sx={{backgroundColor:"#416DD1"}}
-        >
-         <LinkedInIcon />
-      </Button>
 
-      <Button
-        variant="contained"
-        sx={{backgroundColor:"#416DD1"}}
-        onClick={handleGithubClic}
-        >
-        <GitHubIcon />
-      </Button>
-       <Button
-          variant="contained"
-          sx={{backgroundColor:"#416DD1"}}
-          onClick={handleEmailClick}
-        >
-        <MailOutlineIcon />   
-     </Button>
-    </Box>
     </Box>
   </Box>
 )
