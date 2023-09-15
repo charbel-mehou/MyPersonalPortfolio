828fc2400a225a27b7ec24a08a3a96af4b980d50
charbel-mehou <charbel.mehou@groupe-esigelec.org>
2023-09-16 00:05:32 +0200

    ✨update box to grid

--- personal_portfolio/src/components/SkillsDetails.js
+++ personal_portfolio/src/components/SkillsDetails.js
@@ -1,6 +1,4 @@
-import { Box, Typography } from "@mui/material";
 import React from "react";
-import { useTranslation } from "react-i18next";
 import CsharpLogo from "../assets/icons/csharp-icon.png";
 import Css from "../assets/icons/css.png";
 import HmtlLogo from "../assets/icons/html-icon.png";
@@ -10,45 +8,23 @@ import PhpLogo from "../assets/icons/php-icon.png";
 import ReactLogo from "../assets/icons/react-icon.png";
 import Sql from "../assets/icons/sql.png";
 import TypeScriptLogo from "../assets/icons/typescript-icon.png";
-import BoxDetails from "./BoxDetails";
+import NodeLogo from "../assets/icons/nodejs.png";
+import GridDetails from "./GridDetails";
 
 const SkillsDetails = () => {
-  const { t } = useTranslation();
   const skillstab = [
     { logo: CsharpLogo, label: "C#" },
     { logo: ReactLogo, label: "React" },
-    { logo: JsLogo, label: "JavaScript" },
     { logo: TypeScriptLogo, label: "TypeScript" },
-    { logo: PhpLogo, label: "Php" },
+    { logo: JsLogo, label: "JavaScript" },
     { logo: HmtlLogo, label: "Html" },
+    { logo: PhpLogo, label: "Php" },
     { logo: Css, label: "Css" },
     { logo: Java, label: "Java" },
     { logo: Sql, label: "SQL" },
+    { logo: NodeLogo, label: "Node.js" },
   ];
-  return (
-    <BoxDetails className="section-offset" id={"skills"} title={"skills"}>
-      {skillstab.map((skill, index) => (
-        <Box
-          key={index}
-          className="projectCard-item"
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            gap: 1,
-          }}
-        >
-          <img
-            style={{ width: "50px", height: "50px" }}
-            src={skill.logo}
-            alt={skill.label}
-          />
-          <Typography style={{ color: "white" }}>{skill.label}</Typography>
-        </Box>
-      ))}
-    </BoxDetails>
-  );
+  return <GridDetails id={"skills"} title={"skills"} tab={skillstab} />;
 };
 
 export default SkillsDetails;
