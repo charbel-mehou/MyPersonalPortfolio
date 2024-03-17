3b306b213c90640815df751158ab48885d1c682a
charbel-mehou <charbel.mehou@groupe-esigelec.org>
2024-03-17 10:56:30 +0100

    adding new certification

--- personal_portfolio/src/components/CertificationsDetails.js
+++ personal_portfolio/src/components/CertificationsDetails.js
@@ -8,10 +8,11 @@ import ReactCertif from "../assets/images/ReactCertif.png";
 import BoxDetails from "./BoxDetails";
 import Node from '../assets/images/NodeJs.png'
 const certifstab = [
+  { logo: MicrosoftCsharp, label: "C#",link:'https://urlz.fr/pUEw'},
   { logo: ReactCertif, label: "React.js" ,link:'https://urlz.fr/nC41'},
   { logo: JavaScript, label: "Javascript",link:'https://urlz.fr/nC3C' },
   { logo: Csharp, label: "C#",link:'https://urlz.fr/nC3v'},
-  { logo: Node, label: "Node.js",link:'https://cutt.ly/GwbUeUAQ'},
+ // { logo: Node, label: "Node.js",link:'https://cutt.ly/GwbUeUAQ'},
 ];
 const CertificationsDetails = () => {
   const { t } = useTranslation();
